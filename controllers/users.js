e9758b8e043a98b90d87508622d7eda873a85150
Kirill Zhidkov <kirill-info@yandex.ru>
2022-06-19 22:34:43 +0300

    пр13 19-06-2022 4

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,21 @@
 const User = require('../models/user');
 
 module.exports.getUser = (req, res) => {
-  User.findById({ _id: req.params.userId })
-    .then((user) => res.send(user))
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404)
+          .send({ message: 'Передан некорректный _id пользователя' });
+      } else {
+        res.send(user);
+      }
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(404)
+        return res.status(400)
           .send({ message: 'Пользователь по указанному _id не найден' });
-      } return res.status(500).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
     });
 };
 
@@ -35,7 +43,7 @@ module.exports.updateProfile = (req, res) => {
   User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
-      if ((err.name === 'ValidationError') && (err.name === 'SomeError')) {
+      if (err.name === 'ValidationError') {
         return res.status(400)
           .send({ message: 'Переданы некорректные данные при обновлении профиля' });
       }
@@ -53,7 +61,7 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(userId, { avatar }, { new: true })
     .then((user) => res.send(user))
     .catch((err) => {
-      if ((err.name === 'ValidationError') && (err.name === 'SomeError')) {
+      if (err.name === 'ValidationError') {
         return res.status(400)
           .send({ message: 'Переданы некорректные данные при обновлении аватара' });
       }
