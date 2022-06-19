e752fedf1c55f10921ac08775d1b1ac03d38cf63
Kirill Zhidkov <kirill-info@yandex.ru>
2022-06-19 22:55:20 +0300

    пр13 19-06-2022 6

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use((req, res) => {
+  res.status(404)
+    .send({ message: 'Страница не найдена - 404' });
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
